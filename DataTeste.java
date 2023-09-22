df754f1a8b999650560c39d90c734a639acbc182
Vinicius Fontes <130714311+ViniFontes73@users.noreply.github.com>
2023-09-22 16:49:41 -0300

    Create DataTeste.java

---
+++ DataTeste.java
@@ -0,0 +1,11 @@
+package com.mycompany.datateste;
+public class DataTeste {
+
+    public static void main(String[] args) {
+        Data data1 = new Data(07,03,1999);
+        
+        // ----- -EXIBIR --------
+        System.out.println("DATA 1");
+        data1.displayData();
+    }
+}
